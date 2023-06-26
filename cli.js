a2feb29938fa93d2b742013182e191762828df49
Rubyboat1207 <rudy@rudysoliz.com>
2023-06-26 13:56:03 -0500

    cleaned up the code a bit

--- cli.js
+++ cli.js
@@ -1,73 +1,72 @@
 #!/usr/bin/env node
-var utils = require('./index')
-var fs = require('fs')
+
+const utils = require('./index');
+const fs = require('fs');
 
 const typingsDir = process.argv[2] || '';
 
-if(!utils.isASailsProject()) {
-    console.log("This is not a sails.js project")
+if (!utils.isASailsProject()) {
+    console.log("This is not a sails.js project");
     return;
 }
 
-console.log("Creating Model Declarations")
+console.log("Creating Model Declarations");
 
 const attrs = utils.getAllModels();
-// let globalAdditions = '';
-// let imports = [];
-// let vars = '';
+if(!fs.existsSync(`${typingsDir}`))
+    fs.mkdirSync(`${typingsDir}`);
 
-fs.mkdirSync(`${typingsDir}models`);
+if(!fs.existsSync(`${typingsDir}models/`))
+    fs.mkdirSync(`${typingsDir}models`);
 
-for(const model of attrs) {
+for (const model of attrs) {
     const objName = `${model.name}Object`;
-    // globalAdditions += `        ${model.name}: ${model.name}Model\n`;
-    // imports += `import type { ${model.name}Model } from "./${model.name}.d.ts"\n`
-    // vars += `export var ${model.name}: ${model.name}Model;\n`
-
 
     let baseFile = `
 declare interface ${objName} {
-    
-    `
+`;
 
-    for(const key in model.attr) {
-        baseFile += key + ': ';
+    for (const key in model.attr) {
+        baseFile += `    ${key}: `;
 
-        type = model.attr[key].type;
-        allowNull = model.attr[key].allowNull;
-        coltype = model.attr[key].columnType;
+        const type = model.attr[key].type;
+        const allowNull = model.attr[key].allowNull;
+        const coltype = model.attr[key].columnType;
 
-        if(['string', 'number', 'boolean'].includes(type)) {
-            if(coltype === 'datetime') {
+        if (['string', 'number', 'boolean'].includes(type)) {
+            if (coltype === 'datetime') {
                 baseFile += 'Date';
-            }else {
-                baseFile += type
+            } else {
+                baseFile += type;
             }
-        }else {
-            baseFile += 'any'
+        } else {
+            baseFile += 'any';
         }
 
-        if(allowNull === true || allowNull === undefined) {
+        if (allowNull === true || allowNull === undefined) {
             baseFile += ' | null';
         }
 
-        baseFile += ';\n    '
+        baseFile += ';\n';
     }
 
     baseFile += `
 }
-export declare interface ${model.name}Model {
-    update(selector: ${objName}): {set: (values: ${objName}) => Promise< void >}
-    findOne(selector: ${objName}): Promise< ${objName} >
-    find(selector: ${objName}): Promise< ${objName}[] >
-}`
 
-    fs.writeFileSync(`./${typingsDir}models/${model.name}.d.ts`, baseFile)
-    console.log(`Generated ${model.name} declaration`)
+export declare interface ${model.name}Model {
+    update(selector: ${objName}): { set: (values: ${objName}) => Promise<void> };
+    findOne(selector: ${objName}): Promise<${objName}>;
+    find(selector: ${objName}): Promise<${objName}[]>;
+}`;
+
+    fs.writeFileSync(`./${typingsDir}models/${model.name}.d.ts`, baseFile);
+    console.log(`Generated ${model.name} declaration`);
 }
 
+const importStatements = attrs.map((m) => `import type { ${m.name}Model } from "./models/${m.name}.d.ts";`).join('\n');
+
 let globalDecl = `
-${attrs.map((m) => `import type { ${m.name}Model } from "./models/${m.name}.d.ts"`).join('\n')}
+${importStatements}
 
 declare interface SailsObject {
     helpers: HelpersObject;
@@ -85,12 +84,11 @@ declare global {
     ${attrs.map((m) => `export var ${m.name}: ${m.name}Model;`).join('\n    ')}
 }
 
-
-
 declare namespace NodeJS {
     interface Global {
         sails: SailsObject;
-        ${attrs.map((m) => `${m.name}: ${m.name}Model`).join('\n        ')}
+        ${attrs.map((m) => `${m.name}: ${m.name}Model;`).join('\n        ')}
     }
-}`
-    fs.writeFileSync(`${typingsDir}global.d.ts`, globalDecl);
+}`;
+
+fs.writeFileSync(`${typingsDir}global.d.ts`, globalDecl);
